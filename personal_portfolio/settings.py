b424c1f3b041d8f9588fe0527aaa2e763575c95d
Gabriel Santos <gabriel_santos@discente.ufg.br>
2020-10-29 16:50:48 -0300

    update settings.py
    change database to postgress

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -89,12 +89,17 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
